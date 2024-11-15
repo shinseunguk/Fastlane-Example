e365f88c0114211f8725fb2586b0acd28b883ec9
신승욱 <ukseung.dev@incross.com>
2024-11-15 15:37:39 +0900

    Initial Commit

---
+++ Fastlane-Example/Fastlane_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fastlane_ExampleApp.swift
+//  Fastlane-Example
+//
+//  Created by ukseung.dev on 11/15/24.
+//
+
+import SwiftUI
+
+@main
+struct Fastlane_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
